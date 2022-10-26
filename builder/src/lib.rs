62d5df0da1be10856add59668f251fe4d0810d7e
Plasmatium <slv309@163.com>
2022-10-26 22:15:32 +0800

    finished stage 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,39 +1,29 @@
 use proc_macro::TokenStream;
 
-use syn::{parse_macro_input, DeriveInput, Data};
-use quote::{quote, format_ident};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Field};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree.
     let input = parse_macro_input!(input as DeriveInput);
-    let name = input.ident;
 
     // gen Builder struct
-    let builder_struct = gen_builder_struct(&name.to_string(), &input.data);
+    let builder_struct = gen_builder_struct(&input);
+    let implementation = gen_impl(&input);
 
     let expanded = quote! {
         #builder_struct
-
-        impl #name {
-            pub fn builder() {}
-        }
+        #implementation
     };
-    
+
     proc_macro::TokenStream::from(expanded)
 }
 
-fn gen_builder_struct(origin_name: &str, data: &Data) -> proc_macro2::TokenStream {
-    // make fields
-    let struct_data = match data {
-        Data::Struct(s) => s,
-        _ => panic!("Builder must be a named fields struct")
-    };
-    let named_fields = match &struct_data.fields {
-        syn::Fields::Named(n) => n,
-        _ => panic!("Builder must be a named fields struct")
-    };
-    let fields_iter = named_fields.named.iter().map(|f| {
+fn gen_builder_struct(input: &DeriveInput) -> proc_macro2::TokenStream {
+    let (origin_name, _) = gen_name(input);
+    let field_iter = get_field_iter(input);
+    let opt_field_iter = field_iter.map(|f| {
         let f = f.clone();
         let field_name = f.ident.expect("named field");
         let typ = f.ty;
@@ -41,38 +31,54 @@ fn gen_builder_struct(origin_name: &str, data: &Data) -> proc_macro2::TokenStrea
             #field_name: Option<#typ>,
         }
     });
-    
+
     // make builder struct name
     let builder_name = format_ident!("{origin_name}Builder");
 
     quote! {
+        #[derive(Debug)]
         pub struct #builder_name {
-            #(#fields_iter)*
+            #(#opt_field_iter)*
         }
     }
 }
 
-fn gen_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
-
-    todo!();
+fn gen_name(input: &DeriveInput) -> (proc_macro2::Ident, proc_macro2::Ident) {
+    let origin_name = input.ident.clone();
+    let builder_name = format_ident!("{origin_name}Builder");
+    (origin_name, builder_name)
 }
 
-fn get_fields_iter(input: &DeriveInput) -> impl Iterator<Item = proc_macro2::TokenStream> {
+fn gen_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
+    let (origin_name, builder_name) = gen_name(input);
+    let field_iter = get_field_iter(input);
+    let field_asign_none_value = field_iter.map(|f| {
+        let field = f.clone();
+        let name = field.ident;
+        quote! {
+            #name: None,
+        }
+    });
+    quote! {
+        impl #origin_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#field_asign_none_value)
+                    *
+                }
+            }
+        }
+    }
+}
+
+fn get_field_iter(input: &DeriveInput) -> impl Iterator<Item = &Field> {
     let struct_data = match &input.data {
         Data::Struct(s) => s,
-        _ => panic!("Builder must be a named fields struct")
+        _ => panic!("Builder must be a named fields struct"),
     };
     let named_fields = match &struct_data.fields {
         syn::Fields::Named(n) => n,
-        _ => panic!("Builder must be a named fields struct")
+        _ => panic!("Builder must be a named fields struct"),
     };
-    let fields_iter = named_fields.named.iter().map(|f| {
-        let f = f.clone();
-        let field_name = f.ident.expect("named field");
-        let typ = f.ty;
-        quote! {
-            #field_name: Option<#typ>,
-        }
-    });
-    fields_iter
+    named_fields.named.iter()
 }
