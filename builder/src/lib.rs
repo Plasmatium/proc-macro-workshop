346d10444435621413b0a98f3be6e6de057cec96
Jonny <junyu.wang@aipocloud.com>
2022-10-26 17:45:16 +0800

    stage 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,53 @@
 use proc_macro::TokenStream;
 
+use syn::{parse_macro_input, DeriveInput, Data};
+use quote::{quote, format_ident};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree.
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
 
-    unimplemented!()
+    // gen Builder struct
+    let builder_struct = gen_builder_struct(&name.to_string(), &input.data);
+
+    let expanded = quote! {
+        #builder_struct
+
+        impl #name {
+            pub fn builder() {}
+        }
+    };
+    
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn gen_builder_struct(origin_name: &str, data: &Data) -> proc_macro2::TokenStream {
+    // make fields
+    let struct_data = match data {
+        Data::Struct(s) => s,
+        _ => panic!("Builder must be a named fields struct")
+    };
+    let named_fields = match &struct_data.fields {
+        syn::Fields::Named(n) => n,
+        _ => panic!("Builder must be a named fields struct")
+    };
+    let fields_iter = named_fields.named.iter().map(|f| {
+        let f = f.clone();
+        let field_name = f.ident.expect("named field");
+        let typ = f.ty;
+        quote! {
+            #field_name: Option<#typ>,
+        }
+    });
+    
+    // make builder struct name
+    let builder_name = format_ident!("_{origin_name}");
+
+    quote! {
+        pub struct #builder_name {
+            #(#fields_iter)*
+        }
+    }
 }
